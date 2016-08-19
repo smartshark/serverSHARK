06577b3b5baced8ba169ed9b146c313e5eaaf95f
Fabian Trautsch <fabian.trautsch@informatik.uni-goettingen.de>
2016-08-19 15:54:59 +0200

    deployment fixes

--- server/base.py
+++ server/base.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Application definition
