c7a485d470c18110f71de7b7202d33bff52259d8
Fabian Trautsch <fabian.trautsch@informatik.uni-goettingen.de>
2016-08-22 16:04:13 +0200

    first ansible deployment

---
+++ deployment/templates/mysql_dump.sql
@@ -0,0 +1,2 @@
+INSERT INTO `smartshark_smartsharkuser_roles` (`id`, `smartsharkuser_id`, `mongorole_id`) VALUES (NULL, '1', '1');
+INSERT INTO `smartshark_smartsharkuser_roles` (`id`, `smartsharkuser_id`, `mongorole_id`) VALUES (NULL, '1', '2');
