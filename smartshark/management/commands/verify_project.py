b5ba23978069cd501b2d58cb56d413930e986ca5
Benjamin Ledel <benjamin.ledel@stud.uni-goettingen.de>
2019-02-13 00:46:19 -0800

    * Review

--- smartshark/management/commands/verify_project.py
+++ smartshark/management/commands/verify_project.py
@@ -52,7 +52,7 @@ class Command(BaseCommand):
                 resultModel.commmit = str(commit)
                 resultModel.text = ""
 
-                db_commit = self.get_commit_from_database(commit)
+                db_commit = self.get_commit_from_database(commit, vcsMongo["_id"])
 
                 # Basic validation wihtout checkout the version
                 resultModel.vcsSHARK = self.validate_vcsSHARK(db_commit, repo, resultModel)
@@ -77,11 +77,13 @@ class Command(BaseCommand):
         print("validation complete")
 
     # Get commit form database
-    def get_commit_from_database(self, commitHex):
-        return self.db.commit.find_one({"revision_hash": commitHex})
+    def get_commit_from_database(self, commitHex, vcs_system_id):
+        return self.db.commit.find_one({"revision_hash": commitHex, 'vcs_system_id': vcs_system_id})
 
 
-
+    # Get commit form database
+    def get_code_entities_from_database(self, list_of_ids):
+        return self.db.code_entity_state.find({"_id" : {"$in" : list_of_ids}})
 
     # Plugins validation methods
     def validate_vcsSHARK(self, commit, repo, resultModel):
@@ -201,7 +203,9 @@ class Command(BaseCommand):
         code_entity_state_coastSHARK = []
         code_entity_state_mecoSHARK = []
 
-        for db_code_entity_state in db_commit["code_entity_states"]:
+        list_code_entity = self.get_code_entities_from_database(db_commit["code_entity_states"])
+
+        for db_code_entity_state in list_code_entity:
             if db_code_entity_state["ce_type"] == 'file':
                 self.validate_coastSHARK(db_code_entity_state, code_entity_state_coastSHARK)
                 self.validate_mecoSHARK(db_code_entity_state, code_entity_state_mecoSHARK)
