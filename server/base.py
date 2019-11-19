712a346633dfa681aca9414eb775853cf9ba86f9
atrautsch <alexander.trautsch@cs.uni-goettingen.de>
2019-11-19 15:02:29 +0100

    add readabilitySHARK to autoinstall

--- server/base.py
+++ server/base.py
@@ -202,4 +202,4 @@ LOGGING = {
     }
 }
 
-PLUGIN_URLS = ["https://github.com/smartshark/vcsSHARK","https://github.com/smartshark/coastSHARK", "https://github.com/smartshark/mecoSHARK","https://github.com/smartshark/issueSHARK","https://github.com/smartshark/mailingSHARK","https://github.com/smartshark/labelSHARK","https://github.com/smartshark/refSHARK","https://github.com/smartshark/linkSHARK","https://github.com/smartshark/changeSHARK","https://github.com/smartshark/inducingSHARK"]
+PLUGIN_URLS = ["https://github.com/smartshark/vcsSHARK","https://github.com/smartshark/coastSHARK", "https://github.com/smartshark/mecoSHARK","https://github.com/smartshark/issueSHARK","https://github.com/smartshark/mailingSHARK","https://github.com/smartshark/labelSHARK","https://github.com/smartshark/refSHARK","https://github.com/smartshark/linkSHARK","https://github.com/smartshark/changeSHARK","https://github.com/smartshark/inducingSHARK", "https://github.com/smartshark/readabilitySHARK"]
