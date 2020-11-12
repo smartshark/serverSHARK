7993567fffb86d05fcd7cef280fe104693e08f42
atx <atx@drecks-provider.de>
2020-11-12 14:32:55 +0100

    add new database design image, add command to add schemas to documentation outside of plugins

---
+++ smartshark/management/commands/add_schema_documentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import json
+
+from django.core.management.base import BaseCommand
+
+from smartshark.mongohandler import handler
+
+
+logger = logging.getLogger('django')
+
+
+class Command(BaseCommand):
+    """Adds additional schema documentation that is not part of plugins"""
+
+    help = 'Adds additional schema documentation that is not part of plugins'
+
+    def handle(self, *args, **options):
+
+        self.stdout.write('adding additional schema information for visualSHARK')
+        with open('./add_schema/visualSHARK.json', 'r') as f:
+            schema = json.loads(f.read())
+            handler.add_schema(schema, 'visualSHARK')
+        self.stdout.write('additional schema information written.')
