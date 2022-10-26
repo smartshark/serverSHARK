fd3f835e8065153264b1d7ad5cb4555f1589a2cd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-26 01:36:05 +0000

    Adding tarfile member sanitization to extractall()

--- smartshark/datacollection/localqueueconnector.py
+++ smartshark/datacollection/localqueueconnector.py
@@ -101,7 +101,26 @@ class LocalQueueConnector(PluginManagementInterface, BaseConnector):
 
                 # Extract it
                 with tarfile.open(tmp_tar_gz, "r:gz") as tar_gz:
-                    tar_gz.extractall(self.project_path)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar_gz, self.project_path)
 
                 # Delete temporary tar_gz
                 os.remove(tmp_tar_gz)
